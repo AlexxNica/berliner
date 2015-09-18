77ff007a0c909a6756df883d4d6e0934b63721de
Seth Thompson <s3th.thompson@gmail.com>
2015-09-17 23:12:10 -0700

    add to/from JSON serializers

---
+++ renderers/json.go
@@ -0,0 +1,18 @@
+package renderers
+
+import (
+	"io/ioutil"
+	"encoding/json"
+
+	"github.com/s3ththompson/berliner/content"
+)
+
+func ToJSON(filename string) func([]content.Post) {
+	return func(posts []content.Post) {
+		p, err := json.Marshal(posts)
+		if err != nil {
+			return
+		}
+		_ = ioutil.WriteFile(filename, p, 0644)
+	}
+}
---
+++ sources/json.go
@@ -0,0 +1,32 @@
+package sources
+
+import (
+	"io/ioutil"
+	"encoding/json"
+
+	"github.com/s3ththompson/berliner/content"
+	"github.com/s3ththompson/berliner/scrape"
+)
+
+// TODO: change to FromFile and add FromStdin
+func FromJSON(filename string) func(*scrape.Client) <-chan content.Post {
+	return func(c *scrape.Client) <-chan content.Post {
+		out := make(chan content.Post)
+		go func() {
+			defer close(out)
+			p, err := ioutil.ReadFile(filename)
+			if err != nil {
+				return
+			}
+			posts := make([]content.Post, 10)
+			err = json.Unmarshal(p, &posts)
+			if err != nil {
+				return
+			}
+			for _, post := range posts {
+				out <- post
+			}
+		}()
+		return out
+	}
+}
