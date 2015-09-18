4c616d6f71bbb977448674e463055de115fabdb0
Seth Thompson <s3th.thompson@gmail.com>
2015-09-17 23:13:24 -0700

    add dedupe and read-time filters

---
+++ filters/dedupe.go
@@ -0,0 +1,77 @@
+package filters
+
+import (
+	"os"
+	"bufio"
+	"github.com/s3ththompson/berliner/content"
+)
+
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func writeLines(file string, lines []string) error {
+    f, err := os.Create(file)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+    w := bufio.NewWriter(f)
+    defer w.Flush()
+    for _, line := range lines {
+        _, err := w.WriteString(line + "\n")
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func stringInSlice(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
+}
+
+func Dedupe(filenames ...string) func(<-chan content.Post) <-chan content.Post {
+	return func(posts <-chan content.Post) <-chan content.Post {
+		var seen []string
+		persist := false
+		
+		var filename string
+		if len(filenames) > 0 {
+			persist = true
+			filename = filenames[0]
+			seen, _ = readLines(filename)
+		}
+
+		out := make(chan content.Post)
+		go func() {
+			defer close(out)
+			for post := range posts {
+				if !stringInSlice(post.Permalink, seen) {
+					out <- post
+					seen = append(seen, post.Permalink)
+				}
+			}
+			if persist {
+				writeLines(filename, seen)
+			}
+		}()
+		return out
+	}
+}
---
+++ filters/readfor.go
@@ -0,0 +1,38 @@
+package filters
+
+import (
+	"strings"
+	"time"
+
+	"github.com/s3ththompson/berliner/content"
+)
+
+func countWords(text string) int {
+	return len(strings.Fields(text)) // TODO: just look at words in paragraph tags
+}
+
+func ReadFor(duration time.Duration, wpms ...int) func(<-chan content.Post) <-chan content.Post {
+	return func(posts <-chan content.Post) <-chan content.Post {
+		wpm := 250
+		if len(wpms) > 0 {
+			wpm = wpms[0]
+		}
+		minutes := int(duration/time.Minute)
+		maxWords := wpm * minutes
+
+		out := make(chan content.Post)
+		go func() {
+			defer close(out)
+			for post := range posts {
+				words := countWords(post.Body)
+				if words < maxWords {
+					out <- post
+					maxWords -= words
+				} else {
+					return
+				}
+			}
+		}()
+		return out
+	}
+}
