4364f19dbc3b2e652c83e98de61ce9eebd92f853
Geoffrey Litt <gklitt@gmail.com>
2015-09-24 23:55:39 -0400

    fix up pitchfork source

--- scrape/@pitchfork.com.go
+++ scrape/@pitchfork.com.go
@@ -14,13 +14,15 @@ func (s *pitchfork) recognize(url string) bool {
 
 func (s *pitchfork) scrape(page *html.Node) (content.Post, error) {
 	doc := goquery.NewDocumentFromNode(page)
-	infoDiv := doc.Find("#content #Main .info")
-	body, _ := doc.Find(".object-detail .editorial").Html()
+	infoDiv := doc.Find("#content #main .info")
+	article, _ := doc.Find(".object-detail .editorial").Html()
+	score := infoDiv.Find("span.score").Text()
+	body := "<h2>Score: " + score + "</h2>" + article
 
 	return content.Post{
-		Title:    infoDiv.Find("h1").Text() + ": " + infoDiv.Find("h2").Text(), // artist + album name
+		Title:    infoDiv.Find("h1").First().Text() + ": " + infoDiv.Find("h2").Text(), // artist + album name
 		Body:     body,
-		Authors:  []string{infoDiv.Find("h4 a").Text()}, // TODO this should work but doesn't, debug
+		Authors:  []string{infoDiv.Find("h4 a").Text()},
 		Tags:     nil,
 		Origin:   "Pitchfork",
 		Language: doc.Find("html").AttrOr("lang", "en"),
